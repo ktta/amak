4d7b7912a58ac40938e7ae543eb9ccec9ef2c7ef
Sukru Cinar <cinarus@yahoo.com>
2024-02-29 16:53:08 +0300

    clean up

--- mkapk.c
+++ mkapk.c
@@ -1,24 +1,23 @@
 
 void make_apk(int force)
 {
-  int i;
-  varset_t *V;
-  char *flags;
-  target_t *target;
   char *apkfile, *keystore;
-  char *apkpath;
+  int i;
+  target_t *target;
 
   apkfile= ini_getstr(cfg, "project", "apkfile",  DEFVAL_APKFILE);
   keystore= ini_getstr(cfg, "apksigner", "keystore",  DEFVAL_KEYSTORE);
 
   if (!force)
   {
+    char *apkpath;
     apkpath= mkpath( "build" , apkfile, NULL );
-    if (reqisnewer(apkpath, aPT_RESOURCESZIP)) goto doit;
-    if (reqisnewer(apkpath, aPT_CLASSESDEX))   goto doit;
+    if (stale(apkpath, keystore)
+        || stale(apkpath, aPT_RESOURCESZIP) 
+        || stale(apkpath, aPT_CLASSESDEX)) goto doit;
     for(i=0;target=cvars.targets[i];i++)
     {
-      if (reqisnewer(apkpath, mkpath(target->libdir, aFN_LIBMAINSO, NULL)))
+      if (stale(apkpath, mkpath(target->libdir, aFN_LIBMAINSO, NULL)))
          goto doit;
     }
     return ;
@@ -44,11 +43,15 @@ doit:
   vexec(tool_path("zipalign"), "-f", "4", aPT_UNALIGNEDAPK, aPT_UNSIGNEDAPK,
                                NULL, "aligning apk\n");
   
-  V= varset_cvars();
-  varset_put(V, "apkfile", apkfile);
-  varset_put(V, "keystore", keystore);
-  varset_put(V, "storepass", ini_getstr(cfg, "apksigner", "storepass",
-                                         DEFVAL_STOREPASS));
-  flags= ini_getstr(cfg, "apksigner", "flags", DEFVAL_APKSIGNER_FLAGS);
-  vexec(tool_path("apksigner"), VXMA, seprep(flags,V), NULL, "signing apk\n");
+  vexec(tool_path("apksigner"),
+        VXMA, seprep(ini_getstr(cfg, "apksigner", "flags",
+                                      DEFVAL_APKSIGNER_FLAGS),
+                     varset_putm(varset_cvars(),
+                                 "apkfile", apkfile,
+                                 "keystore", keystore,
+                                 "storepass", ini_getstr(cfg, "apksigner",
+                                                         "storepass",
+                                                         DEFVAL_STOREPASS),
+                                  NULL)),
+        NULL, "signing apk\n");
 }
